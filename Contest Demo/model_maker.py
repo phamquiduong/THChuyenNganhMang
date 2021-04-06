e53049ad10d8c4f96afbd218bd258858e0a8bcb1
truongquanghung <truongquanghunghn@gmail.com>
2021-04-06 10:03:03 +0700

    Full Contest Demo with instruction

---
+++ Contest Demo/model_maker.py
@@ -0,0 +1,20 @@
+from sklearn import svm
+import pickle
+import os
+data = []
+label = []
+for line in open('data_train.txt', 'r'):
+    words = line.strip().split()
+    label.append(float(words[0]))
+    text = []
+    for i in range(1,len(words)):
+        text.append(float(words[i]))
+    data.append(text)
+
+# Build model
+svc = svm.SVC(C=1, kernel="rbf", gamma=0.1)
+svc.fit(data,label)
+
+# Save model
+s=input()
+pickle.dump(svc,open(os.path.join(s+".pkl"), 'wb'))
