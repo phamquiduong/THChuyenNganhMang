2b476ac96112e50213c4f901672c9e2ce640bdf2
tueanhtruong <tueleesin@gmail.com>
2021-04-06 23:43:08 +0700

    Add dev Page and Login Page

--- ContestAi/devs/urls.py
+++ ContestAi/devs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dev/',  views.Index.as_view(), name='index'),
+    path('login/',  views.Login.as_view(), name='login'),
+
 ]
--- ContestAi/devs/views.py
+++ ContestAi/devs/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 from django import template
+from . import models
 
 
 class Index(View):
-    template = 'index.html'
+    template = 'index/index.html'
+    obj = models.Dev.objects.get(id=1)
+    context = {
+        "name": "Tue Anh Truong",
+        "language": ["Python", "JS", "C#", "C++", "Java"],
+        "condition": False,
+        "data": obj,
+    }
+
+    def get(self, request):
+        return render(request, self.template, self.context)
+
+
+class Login(View):
+    template = 'login/login.html'
 
     def get(self, request):
         return render(request, self.template)
