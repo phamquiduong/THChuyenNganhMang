82ada83b9f84070dce88bb66bff4f82ae542872a
truongquanghung <truongquanghunghn@gmail.com>
2021-04-07 17:29:05 +0700

    delete devs and add sklearn for requirements.txt

--- devs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Dev (models.Model):
-    userName = models.TextField()
-    userDescription = models.TextField()
-    userAge = models.TextField(default='25 noi banh chung')
