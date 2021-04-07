81ca7c465e03f418f16fba3b2371dbe97b48d629
tueanhtruong <tueleesin@gmail.com>
2021-04-07 22:39:05 +0700

    Contact with static file

--- ContestAi/ContestAi/settings.py
+++ ContestAi/ContestAi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,4 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-print('BASE_DIR', BASE_DIR)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
