5f18f2db017c702c475bd961bc864387328f4da9
tueanhtruong <tueleesin@gmail.com>
2021-04-10 00:29:12 +0700

    Handle SignUp Flow

--- ContestAi/Auth/models.py
+++ ContestAi/Auth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Auth (models.Model):
+    userName = models.CharField(max_length=50, primary_key=True)
+    userDescription = models.CharField(max_length=50)
+    password =  models.CharField(max_length=50)
--- ContestAi/Auth/views.py
+++ ContestAi/Auth/views.py
@@ -1,18 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from django import template
 from . import models
 
 
+templateLogin = 'login/login.html'
+templateSignUp = 'signUp/signUp.html'
 class Login(View):
-    template = 'login/login.html'
 
     def get(self, request):
-        return render(request, self.template)
+        return render(request, templateLogin)
 
 
 class SignUp(View):
-    template = 'signUp/signUp.html'
 
     def get(self, request):
-        return render(request, self.template)
+        return render(request, templateSignUp)
+
+    def post(seft, request):
+        name = request.POST.get("name")
+        userName = request.POST.get("userName")
+        password = request.POST.get("password")
+        try:
+            p = models.Auth.objects.create(userName=userName, userDescription= name, password=password)
+            p.save()
+            context = {
+                'mess':'Sign Up Success',
+            }
+            return render(request, templateLogin, context);
+        except Exception as e:
+            context = {
+                'mess':'Sign Up Fail',
+            }
+            return render(request, templateSignUp, context)
+
