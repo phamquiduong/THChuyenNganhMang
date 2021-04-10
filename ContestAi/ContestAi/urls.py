8ca755c2a4712c98e386ab37293d26bf279484a9
caoquangtrong <caoquangtrong11a1@gmail.com>
2021-04-10 17:03:18 +0700

    Create ALL Models, CURD class Status

--- ContestAi/ContestAi/urls.py
+++ ContestAi/ContestAi/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('devs.urls')),
     path('', include('Auth.urls')),
-]
+    path('ContestAdmin/', include('ContestAdmin.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
