6abd3ec16ded2fa91eec2a8e4892277d1b62d040
duy <duypoliwhirl@gmail.com>
2021-04-13 21:17:22 +0700

    login+logout

--- ContestAi/Auth/views.py
+++ ContestAi/Auth/views.py
@@ -1,37 +1,34 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, HttpResponse
 from django.views import View
 from django import template
 from . import models
 from service import path,session
-
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 class Login(View):
 
     def get(self, request):
-        if not session.isAuthenticated(request):
-            messAuth = str()
-            try:
-                messAuth = request.session.get('messAuth')
-                del request.session['messAuth']
-            except:
-                messAuth = ''
-            context = {
-                'mess': messAuth,
+        if request.user.is_authenticated == True:
+            logout(request)
+        return render(request, path.templateLogin)
+    def post(self, request):
+        us = request.POST.get("UserName")
+        pw = request.POST.get("PassWord")
+        my_user = authenticate(request, username = User.objects.get(email = us).username, password = pw)
+        if my_user is None:
+            contest = {
+                'mess': 'Tai khoan khong hop le'
             }
-            return render(request, path.templateLogin, context)
+            return render(request, path.templateLogin, contest)
         else:
-            return redirect('holder')
-    def post(seft, request):
-        userName = request.POST.get("userName")
-        password = request.POST.get("password")
-        request.session['user'] = {
-            'name' : userName,
-        }
-        #do sth 
-        #case success login
-        return redirect('holder')
-
+            login(request,my_user)
+            if request.user.is_superuser == True:
+                return HttpResponse('Welcome'+request.user.username)
+            elif request.user.is_staff == True:
+                return HttpResponse('Hello'+request.user.username)
+            elif request.user.is_active == True:
+                return HttpResponse('Hi'+request.user.username) 
 
 
 class SignUp(View):
@@ -51,14 +48,13 @@ class SignUp(View):
         else:
             return redirect('holder')
 
-    def post(seft, request):
+    def post(self, request):
         name = request.POST.get("name")
         userName = request.POST.get("userName")
         password = request.POST.get("password")
         roleId = request.POST.get("role")
         try:
-            p = models.Auth.objects.create(userName=userName, userDescription= name, password=password)
-            p.save()
+            
             request.session['messAuth'] = 'Sign Up Success'
             return redirect('../login')
         except Exception as e:
@@ -67,10 +63,5 @@ class SignUp(View):
 
 class LogOut(View):
     def post(self, request):
-        try:
-            request.session['messAuth'] = 'Log Out Success'
-            del request.session['user']
-        except:
-            request.session['messAuth'] = 'Log Out Fail'
-            return redirect('login')
+        logout(request)
         return redirect('login')
