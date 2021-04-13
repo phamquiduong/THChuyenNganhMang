bf965058a974315a0febec48bede7503f6d6e62e
Phạm Quí Dương <62789707+phamquiduong@users.noreply.github.com>
2021-04-14 00:24:05 +0700

    Tạo database test

--- ContestAi/ContestParticipant/views.py
+++ ContestAi/ContestParticipant/views.py
@@ -1,26 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import random
-from django.core.files.storage import FileSystemStorage
+import sqlite3
+
+# import random
+# from django.core.files.storage import FileSystemStorage
 
 
 # Create your views here.
 
 def index(request):
+        #update Profile
     if request.method == 'POST':
-        upload_file = request.FILES['submit_file']
-        print(upload_file.name)
-        print(upload_file.size)
-        upload_file.save()
-        i = random.randint(0, 1000000)
+        print("Update Profile comming soon")
+        return
+        
+
+    haveError = False
+        #Select Database
+    try:
+        conn = sqlite3.connect('./db.sqlite3')
+
+        cmd = 'SELECT * FROM ContestAdmin_account'
+        contestSubmited = conn.execute(cmd)
+        
+        cmd = 'SELECT * FROM ContestAdmin_account'
+        contestRegisted = conn.execute(cmd)
+        
+        cmd = 'SELECT * FROM ContestAdmin_account'
+        contestAll = conn.execute(cmd)
+    except EOFError as e:
+        print(e)
+        haveError = True
+
+
+    
+        #Render Site
+    dic = {'haveError': haveError, 'contestSubmited': contestSubmited, 'contestRegisted': contestRegisted, 'contestAll': contestAll}
+    return render(request, 'ContestParticipant/index.html', dic)
 
 
 
 
-
-
-    # title = 'Đây là cái tiêu đề'
-    # link_pdf = 'https://vinasupport.com/my_pdf_file.pdf'
-    # luu_y = ['1, ABC', '2, XYZ']
-    # dic = {'title': title, 'link_pdf': link_pdf, 'luu_y': luu_y}
-    return render(request, 'ContestParticipant/index.html')
+def error(request):
+    return render(request, 'ContestParticipant/error.html')
