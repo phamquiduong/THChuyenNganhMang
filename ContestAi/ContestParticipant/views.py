2953543ed8144fa6eaa6322695f9b1e9ff29327c
Phạm Quí Dương <62789707+phamquiduong@users.noreply.github.com>
2021-04-14 08:53:27 +0700

    View data

--- ContestAi/ContestParticipant/views.py
+++ ContestAi/ContestParticipant/views.py
@@ -24,23 +24,17 @@ def index(request):
         cmd = 'SELECT * FROM ContestAdmin_status JOIN ContestAdmin_contest ON ContestAdmin_status.IDcontest = ContestAdmin_contest.id JOIN ContestAdmin_language ON ContestAdmin_language.id = ContestAdmin_contest.IDLanguage WHERE ContestAdmin_status.IDUser = ' + id
         contestSubmited = conn.execute(cmd)
         
-        cmd = 'SELECT * FROM ContestAdmin_account'
+        cmd = 'SELECT * FROM ContestAdmin_registercontest JOIN ContestAdmin_contest ON ContestAdmin_contest.id = ContestAdmin_registercontest.IDContest JOIN ContestAdmin_language ON ContestAdmin_contest.IDLanguage = ContestAdmin_language.id WHERE ContestAdmin_registercontest.IDUser = ' + id + ' AND ContestAdmin_registercontest.IDcontest NOT IN (SELECT (IDcontest) FROM ContestAdmin_status WHERE IDUser = ' + id + ')'
         contestRegisted = conn.execute(cmd)
         
-        cmd = 'SELECT * FROM ContestAdmin_account'
+        cmd = 'SELECT * FROM ContestAdmin_contest JOIN ContestAdmin_language ON ContestAdmin_contest.IDLanguage = ContestAdmin_language.id WHERE ContestAdmin_contest.id NOT IN (SELECT (IDcontest) FROM ContestAdmin_registercontest WHERE ContestAdmin_registercontest.IDUser = ' + id + ')'
         contestAll = conn.execute(cmd)
     except EOFError as e:
         print(e)
         haveError = True
 
 
-    
+    # haveError = True
         #Render Site
-    dic = {'haveError': haveError, 'contestSubmited': contestSubmited, 'contestRegisted': contestRegisted, 'contestAll': contestAll}
+    dic = {'id': id, 'haveError': haveError, 'contestSubmited': contestSubmited, 'contestRegisted': contestRegisted, 'contestAll': contestAll}
     return render(request, 'ContestParticipant/index.html', dic)
-
-
-
-
-def error(request):
-    return render(request, 'ContestParticipant/error.html')
