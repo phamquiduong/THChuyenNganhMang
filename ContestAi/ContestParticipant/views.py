5e5a52f6f50c1626cd858ddad5f0961df19f86a3
tueanhtruong <tueleesin@gmail.com>
2021-04-26 23:47:41 +0700

    Intergrate Participant page

--- ContestAi/ContestParticipant/views.py
+++ ContestAi/ContestParticipant/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 #from .data import mockContest
 import sqlite3
 from datetime import datetime
+from ContestAdmin import models
+
 
 # import random
 # from django.core.files.storage import FileSystemStorage
@@ -56,88 +58,51 @@ class ParticipantView(View):
         if request.user.is_active:
             try:
                 userName = request.user.username
+                userId = request.user.id
             except:
                 userName = ''
-
+                userId = 0
             try:
-                conn = sqlite3.connect('./db.sqlite3')
 
-                cmd = "SELECT id FROM auth_user WHERE username='{}'".format(userName)
-                data = conn.execute(cmd)
-                for row in data:
-                    id = row[0]
-                print(data,id)
+                id = userId
+                allContest = models.Contest.objects.all()
+                print(allContest)
 
-                cmd = "SELECT ContestAdmin_contest.id,ContestAdmin_contest.Title,ContestAdmin_contest.Description,ContestAdmin_contest.TimeRegister,ContestAdmin_contest.TimeStart,ContestAdmin_contest.TimeEnd,ContestAdmin_language.Language,ContestAdmin_status.Status FROM ContestAdmin_status JOIN ContestAdmin_contest ON ContestAdmin_status.IDcontest = ContestAdmin_contest.id JOIN ContestAdmin_language ON ContestAdmin_language.id = ContestAdmin_contest.IDLanguage WHERE ContestAdmin_status.IDUser = '{}' GROUP BY ContestAdmin_status.IDContest".format(id)
-                submited = conn.execute(cmd)
-        
-                cmd = "SELECT ContestAdmin_contest.id,ContestAdmin_contest.Title,ContestAdmin_contest.Description,ContestAdmin_contest.TimeRegister,ContestAdmin_contest.TimeStart,ContestAdmin_contest.TimeEnd,ContestAdmin_language.Language FROM ContestAdmin_registercontest JOIN ContestAdmin_contest ON ContestAdmin_contest.id = ContestAdmin_registercontest.IDContest JOIN ContestAdmin_language ON ContestAdmin_contest.IDLanguage = ContestAdmin_language.id WHERE ContestAdmin_registercontest.IDUser='{0}' AND ContestAdmin_registercontest.IDcontest NOT IN (SELECT (IDcontest) FROM ContestAdmin_status WHERE IDUser = '{0}')".format(id)
-                registed = conn.execute(cmd)
-        
-                cmd = "SELECT ContestAdmin_contest.id,ContestAdmin_contest.Title,ContestAdmin_contest.Description,ContestAdmin_contest.TimeRegister,ContestAdmin_contest.TimeStart,ContestAdmin_contest.TimeEnd,ContestAdmin_language.Language FROM ContestAdmin_contest JOIN ContestAdmin_language ON ContestAdmin_contest.IDLanguage = ContestAdmin_language.id WHERE ContestAdmin_contest.id NOT IN (SELECT (IDcontest) FROM ContestAdmin_registercontest WHERE ContestAdmin_registercontest.IDUser = '{0}')".format(id)
-                unregisted = conn.execute(cmd)
+                idContestRegis = models.RegisterContest.objects.filter(IDUser=request.user.id).values_list('IDContest',flat=True) # contest that user regis before
+                print(list(idContestRegis))
+                contestRegisted = []
+                for id in list(idContestRegis):
+                    contestRegisted.append(allContest.filter(id=id)[0])
+                print('contestRegisted',contestRegisted)
+                
+                allStatus = models.Status.objects.filter(IDUser=request.user.id)
+                idContestSubmitted = allStatus.values_list('IDcontest',flat=True) # contest that user regis before
+                statusContestSubmitted = allStatus.values_list('Status',flat=True)
+                contestSubmitted = []
+                index = 0
+                for id in list(idContestSubmitted):
+                    selected = allContest.filter(id=id)[0]
+                    selected.Result = statusContestSubmitted[index]
+                    contestSubmitted.append(selected)
+                    index += 1
+                print('contestSubmitted',contestSubmitted)
+                
+                contestUnRegisted = []
+                for contest in allContest:
+                    if contest not in contestRegisted and contest not in contestSubmitted:
+                        contestUnRegisted.append(contest)
+                print('contestUnRegisted', contestUnRegisted)
 
             except Exception as e:
                 print(e)
 
-            historyContests=[]
-            regisContests=[]
-            unRegisContests=[]
-
-            # print('\n\n\n-----------------------Sub contest')
-            for row in submited:    
-                t = {
-                    "idContest": row[0],
-                    "title": row[1],
-                    "description": row[2],
-                    "timeRegister": row[3],
-                    "timeStart": row[4],
-                    "timeEnd": row[5],
-                    "language": row[6],
-                    "result":row[7]
-                }
-                historyContests.append(t)
-            # for x in historyContests: print(x)
-
-            # print('\n\n\n-----------------------Regi contest')
-            for row in registed:
-                t = {
-                    "idContest": row[0],
-                    "title": row[1],
-                    "description": row[2],
-                    "timeRegister": row[3],
-                    "timeStart": row[4],
-                    "timeEnd": row[5],
-                    "language": row[6],
-                    "isRegister": True,
-                }
-                regisContests.append(t)
-            # for x in regisContests: print(x)
-    
-
-            # print('\n\n\n-----------------------All contest')
-            for row in unregisted:   
-                t = {
-                    "idContest": row[0],
-                    "title": row[1],
-                    "description": row[2],
-                    "timeRegister": row[3],
-                    "timeStart": row[4],
-                    "timeEnd": row[5],
-                    "language": row[6],
-                    "isRegister": False,
-                }
-                unRegisContests.append(t)
-            # for x in unRegisContests: print(x)
-            
             context = {
                 'name': userName,
-                'unRegisContests':unRegisContests,
-                'regisContests': regisContests,
-                'historyContests': historyContests,
+                'unRegisContests':list(contestUnRegisted),
+                'regisContests':list(contestRegisted),
+                'historyContests': list(contestSubmitted),
             }
 
-            print(context)
             
             return render(request, path.templateParticipant , context)
         else:
@@ -160,6 +125,7 @@ class Register(View):
         if request.user.is_active:
             try:
                 userName = request.user.username
+                userId = request.user.id
             except:
                 userName = ''
                 
