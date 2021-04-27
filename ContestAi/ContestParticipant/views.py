8e15233c2e063129eb44948f6e9e0b7280788fed
tueanhtruong <tueleesin@gmail.com>
2021-04-27 10:14:11 +0700

    delete unuse code

--- ContestAi/ContestParticipant/views.py
+++ ContestAi/ContestParticipant/views.py
@@ -4,53 +4,10 @@ from django import template
 from service import path,session
 from django.http import HttpResponse
 #from .data import mockContest
-import sqlite3
 from datetime import datetime
 from ContestAdmin import models
 
 
-# import random
-# from django.core.files.storage import FileSystemStorage
-
-
-# Create your views here.
-
-def index(request):
-    id = '1'
-        #update Profile
-    if request.method == 'POST':
-        print("Update Profile comming soon")
-        return
-        
-
-    haveError = False
-        #Select Database
-    try:
-        conn = sqlite3.connect('./db.sqlite3')
-
-        cmd = 'SELECT * FROM ContestAdmin_status JOIN ContestAdmin_contest ON ContestAdmin_status.IDcontest = ContestAdmin_contest.id JOIN ContestAdmin_language ON ContestAdmin_language.id = ContestAdmin_contest.IDLanguage WHERE ContestAdmin_status.IDUser = ' + id
-        contestSubmited = conn.execute(cmd)
-        
-        cmd = 'SELECT * FROM ContestAdmin_account'
-        contestRegisted = conn.execute(cmd)
-        
-        cmd = 'SELECT * FROM ContestAdmin_account'
-        contestAll = conn.execute(cmd)
-    except EOFError as e:
-        print(e)
-        haveError = True
-
-
-    
-        #Render Site
-    dic = {'haveError': haveError, 'contestSubmited': contestSubmited, 'contestRegisted': contestRegisted, 'contestAll': contestAll}
-    return render(request, 'ContestParticipant/index.html', dic)
-
-
-
-
-def error(request):
-    return render(request, 'ContestParticipant/error.html')
 
 
 class ParticipantView(View):
